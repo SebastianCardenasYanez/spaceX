3d03b9b5a3a079efd091c2b28a20801fd7513faa
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-06-27 20:59:59 -0500

    feat: :sparkles: template to center

--- js/componetsModule/templates.js
+++ js/componetsModule/templates.js
@@ -50,8 +50,61 @@ export const leftSection = async (nameRocket) => {
 
 export const centerSection = async (nameRocket) => {
     let plantilla = "";
-    plantilla = /*html*/`
+
+    plantilla += /*html*/`
+    <div class="circular-progress css3"></div>
+
+    <div class="circular-progress css3"></div>
+
+    <div class="circular-progress css3"></div>
     
-        `
+    <article class="article__section_center">
+        <strong class="title__table">Stage 1</strong>
+        <hr>
+        <span>Reusable</span><strong>${nameRocket.docs[0].first_stage.reusable}</strong>
+        <span>Engines</span><strong>${nameRocket.docs[0].first_stage.engines}</strong>
+        <span>Fuel amount</span><strong>${nameRocket.docs[0].first_stage.fuel_amount_tons}</strong>
+        <span>Burn time</span><strong>${nameRocket.docs[0].first_stage.burn_time_sec}</strong>
+    </article>
+    <div class="main__imges" >
+    `;
+
+    let rocket = nameRocket.docs[0].flickr_images
+    rocket.forEach(async images => {
+        plantilla += /*html*/`
+            <img class="images__rockets" src="${images}" referrerpolicy="no-referrer">
+            `
+    })
+
+    plantilla += /*html*/`
+    </div>
+    <article class="article__section_center">
+                <strong class="title__table">Stage 2</strong>
+                <hr>
+                <span>Reusable</span><strong>${nameRocket.docs[0].first_stage.reusable}</strong>
+                <span>Engines</span><strong>${nameRocket.docs[0].first_stage.engines}</strong>
+                <span>Fuel amount</span><strong>${nameRocket.docs[0].first_stage.fuel_amount_tons}</strong>
+                <span>Burn time</span><strong>${nameRocket.docs[0].first_stage.burn_time_sec}</strong>
+            </article>
+
+            <article class="article__section_center">
+                <strong class="title__table">INFORMATION ROCKET</strong>
+                <hr>
+                <span>Type</span><strong>${nameRocket.docs[0].type}</strong>
+                <span>Rocket service</span><strong>${nameRocket.docs[0].active == true ? nameRocket.docs[0].active : "No service"}</strong>
+                <span>Number of stages</span><strong>${nameRocket.docs[0].stages}</strong>
+                <span>Boosters</span><strong>${nameRocket.docs[0].boosters}</strong>
+            </article>
+
+            <article class="article__section_center">
+                <strong class="title__table">ENGINE INFORMATION</strong>
+                <hr>
+                <span>Type</span><strong>${nameRocket.docs[0].engines.type}</strong>
+                <span>Maximum power loss</span><strong>${nameRocket.docs[0].engines.engine_loss_max}</strong>
+                <span>Engine availability</span><strong>${nameRocket.docs[0].engines.layout}</strong>
+                <span>Number of engines</span><strong>${nameRocket.docs[0].engines.number}</strong>
+            </article>
+    `;
+
     return plantilla
 }
