118ddc491393269d81a214dcf39cd976ed96da9b
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-06-28 09:11:51 -0500

    refactor: :recycle: refactorized code
    code refactorized because will be need for others parts in the footer

---
+++ js/componetsModule/getData.js
@@ -0,0 +1,30 @@
+let section__main_left = document.querySelector(".section__main_left");
+let pages = document.querySelectorAll(".page");
+
+export const getNameRocket = async(page) => {
+    console.log(page);
+    let options = {
+        method : "POST",
+        headers : {
+            "content-type" : "application/json"
+        },
+        body : JSON.stringify({
+            options : {
+                page,
+                limit : 1
+            }     
+        })
+    };
+    console.log(options);
+    return fetch("https://api.spacexdata.com/v4/rockets/query", options)
+    .then(data => {
+        if (!data.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return data.json();
+    })
+    .catch(error => {
+        console.error("There was a problem with the fetch operation:", error);
+        throw error;
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import {leftSection, centerSection, rightSection} from "./componetsModule/templates.js";
-import {getNameRocket} from "./module/rockets.js";
+import {templateRocket} from "./module/rockets.js";
 
 let pages = document.querySelectorAll(".page");
 let main__title = document.querySelector(".main__title");
@@ -8,22 +7,4 @@ let section__main_center = document.querySelector(".section__main_center")
 let section__main_left = document.querySelector(".section__main_left");
 let section__main_right = document.querySelector(".section__main_right")
 
-let page;
-let nameRocket;
-
-let num = 0;
-
-pages.forEach(pag => {
-    num++;
-    pag.id = num;
-    console.log(pag)
-    pag.addEventListener("click", async(e) => {
-        page = pag.id
-        nameRocket =await getNameRocket(page);
-        section__main_left.innerHTML = await leftSection(nameRocket);
-        section__main_center.innerHTML = await centerSection(nameRocket);
-        section__main_right.innerHTML = await rightSection(nameRocket);
-        main__title.innerHTML = nameRocket.docs[0].name;
-        console.log(nameRocket)
-    })
-})
+await templateRocket()
