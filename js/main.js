214aeb9102a40188a88a5d0ddfa4b0f73b89a475
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-06-27 20:59:59 -0500

    feat: :sparkles: template to center

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-import {leftSection} from "./componetsModule/templates.js";
+import {leftSection, centerSection} from "./componetsModule/templates.js";
 import {getNameRocket} from "./module/rockets.js";
 
 let pages = document.querySelectorAll(".page");
 let main__title = document.querySelector(".main__title");
+let main__imges = document.querySelector(".main__imges");
+let section__main_center = document.querySelector(".section__main_center")
 
 let page;
 let nameRocket;
@@ -17,7 +19,8 @@ pages.forEach(pag => {
     pag.addEventListener("click", async(e) => {
         page = pag.id
         nameRocket =await getNameRocket(page);
-        section__main_left.innerHTML = await leftSection(nameRocket)
+        section__main_left.innerHTML = await leftSection(nameRocket);
+        section__main_center.innerHTML = await centerSection(nameRocket);
         main__title.innerHTML = nameRocket.docs[0].name;
         console.log(nameRocket)
     })
